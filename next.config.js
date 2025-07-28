ec7d6f4a72c0f99597c0653ce9ba573bb2aef724
Pluto1990 <boomipon@gmail.com>
2025-07-29 07:30:32 +0800

    2025-3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/temple-run-jing' : '',
+  assetPrefix: isProd ? '/temple-run-jing/' : '',
   images: {
     loader: 'akamai',
     path: '',
   },
-  // basePath: '/temple-run-jing',
-  // assetPrefix: '/temple-run-jing',
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
